6b2a88c37d8ced221eae61c7e20e0682f1bcda6c
NotZenix <98800891+NotZenixy@users.noreply.github.com>
2022-03-30 16:40:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,15 +31,15 @@ const config = require("./botconfig/config.json")
 const advertisement = require("./botconfig/advertisement.json")
 const { delay } = require("./handlers/functions")
 require('dotenv').config()
-/port (as described above) and host are both wrong
+//port (as described above) and host are both wrong
 const host = 'localhost';
 const port = 3000;
 
-/use alternate localhost and the port Heroku assigns to $PORT
+//use alternate localhost and the port Heroku assigns to $PORT
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-/Then you can start the server, as usual:
+//Then you can start the server, as usual:
 
 app.listen(port, host, function() {
   console.log("Server started.......");
